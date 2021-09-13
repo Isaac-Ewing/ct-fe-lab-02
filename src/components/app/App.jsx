610d3d3a0b95d5a28de3c689ebfeff804dc099c0
Isaac <isaac.ewing3@gmail.com>
2021-09-13 12:37:55 -0600

    CI passes hopefully

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import AnimalCrossingContainer from '../../containers/AnimalCrossing';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AnimalCrossingContainerDetails from '../../containers/AnimalCrossingDetails';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={AnimalCrossingContainer}/>
-        <Route exact path="/:id" component={AnimalCrossingContainer}/>
+        <Route exact path="/:id" component={AnimalCrossingContainerDetails}/>
       </Switch>
     </BrowserRouter>
   );
--- src/containers/AnimalCrossingDetails.jsx
+++ src/containers/AnimalCrossingDetails.jsx
@@ -3,15 +3,15 @@ import AnimalCrossingCharacterList from '../components/characters/AnimalCrossing
 import AnimalCrossingCharacter from '../components/characters/AnimalCrossingCharacters';
 import { getChars } from '../services/ACApiStuff';
 
-export default class AnimalCrossingContainer extends Component {
+export default class AnimalCrossingContainerDetails extends Component {
     state = {
       characters: []
     }
 
     componentDidMount() {
-        getChars().then((chars) => this.setState({ characters: chars }));
+        getChars(this.props.match.params.id).then((chars) => this.setState({ characters: chars }));
     }
     render() {
-      return <AnimalCrossingCharacterList characters={this.state.characters} />;
+      return <AnimalCrossingCharacter id={this.state.characters.id} name={this.state.characters.name} quote={this.state.characters.quote} personality={this.state.characters.personality} />;
     }
 }
